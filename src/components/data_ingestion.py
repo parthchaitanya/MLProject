4ce97b32b73b393af67f69fe2e147e4d8efb29d7
parthchaitanya <chaitanyaparth.17@gmail.com>
2025-12-23 12:06:27 +0530

    Data Transformatio donen

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,8 +3,13 @@ import sys
 from src.exception import CustomException 
 from src.logger import logging
 import pandas as pd # type: ignore
+
+
 from sklearn.model_selection import train_test_split # type: ignore
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -46,4 +51,11 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()           
+    train_data,test_data=obj.initiate_data_ingestion()    
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
+
+
